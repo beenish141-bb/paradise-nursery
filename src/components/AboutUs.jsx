3b8092ba5b09cb0eb76c4dfadcea725ee389b832
Beenish Bashir <bashirbeenish141@gmail.com>
2026-09-03 19:06:20 +0500

    Add AboutUs component for company introduction

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <section className="about-us">
+      <h1>About Paradise Nursery</h1>
+
+      <p>
+        Paradise Nursery is a company dedicated to providing
+        beautiful and healthy houseplants for homes and offices.
+      </p>
+
+      <p>
+        We offer a variety of indoor plants at affordable prices
+        and aim to make plant shopping simple and enjoyable.
+      </p>
+
+      <p>
+        Our mission is to help customers bring nature into their
+        everyday spaces.
+      </p>
+    </section>
+  );
+}
+
+export default AboutUs;
